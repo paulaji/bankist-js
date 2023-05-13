9dd2651d13984a6e95b421f38b03b50a88f5ddc1
paulaji <paulajiparayil123@gmail.com>
2023-05-13 13:25:59 +0530

    added floor to loan, toFix to all numbers for cleaner look

--- script.js
+++ script.js
@@ -207,7 +207,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value); // rounding off to prev value
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
